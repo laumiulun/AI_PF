fd936f4e56924bfaf1127e92e8c4134baa28e803
Andy Lau <laumiulunn@gmail.com>
2019-06-12 21:12:42 -0600

    added keras test script

---
+++ keras_test.py
@@ -0,0 +1,7 @@
+import numpy as np # For numerical fast numerical calculations
+import matplotlib.pyplot as plt # For making plots
+import pandas as pd # Deals with data
+import seaborn as sns # Makes beautiful plots
+from sklearn.preprocessing import StandardScaler # Testing sklearn
+import tensorflow # Imports tensorflow
+import keras # Imports keras
